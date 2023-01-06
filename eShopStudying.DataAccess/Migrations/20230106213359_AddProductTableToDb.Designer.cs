9cd4df5e86e216a074a3d67d19363afe6e41029b
Yatsyuk Nazar <yatsyuk.nazar@gmail.com>
2023-01-06 23:44:10 +0200

    AddProductTableToDb migrations

--- eShopStudying.DataAccess/Migrations/20230106213359_AddProductTableToDb.Designer.cs
+++ eShopStudying.DataAccess/Migrations/20230106213359_AddProductTableToDb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eShopStudying.DataAccess;
 
@@ -11,9 +12,11 @@ using eShopStudying.DataAccess;
 namespace eShopStudying.DataAccess.Migrations
 {
     [DbContext(typeof(SQLDBContext))]
-    partial class SQLDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230106213359_AddProductTableToDb")]
+    partial class AddProductTableToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
