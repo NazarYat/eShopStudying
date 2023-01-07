0082d7b56629190f110e233fdf03f043444aa857
Yatsyuk Nazar <yatsyuk.nazar@gmail.com>
2023-01-07 23:23:34 +0200

    Updated display names for product model

--- eShopStudying.Models/ProductModel.cs
+++ eShopStudying.Models/ProductModel.cs
@@ -16,23 +16,28 @@ namespace eShopStudying.Models
         public string Author { get; set; }
         [Required]
         [Range(0, 10000)]
+        [Display(Name ="List price")]
         public double ListPrice { get; set; }
         [Required]
         [Range(0, 10000)]
         public double Price { get; set; }
         [Required]
         [Range(0, 10000)]
+        [Display(Name ="Price for 51-100")]
         public double Price50 { get; set; }
         [Required]
         [Range(0, 10000)]
+        [Display(Name ="Price for 100+")]
         public double Price100 { get; set; }
         [Required]
         [ValidateNever]
+        [Display(Name ="Image")]
         public string ImageUrl { get; set; }
-
+        [Display(Name ="Category")]
         public int CategoryId { get; set; }
         [ValidateNever]
         public Category Category { get; set; }
+        [Display(Name ="Cover type")]
         public int CoverTypeId { get; set; }
         [ValidateNever]
         public CoverType CoverType { get; set; }
