ff09be694e30c2a5cc36a6a729a7ad54a50aeb87
Yatsyuk Nazar <yatsyuk.nazar@gmail.com>
2023-01-08 02:54:22 +0200

    Home page

--- eShopStudying/Areas/Customer/Controllers/HomeController.cs
+++ eShopStudying/Areas/Customer/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using eShopStudying.Models;
 using Microsoft.Extensions.Logging;
+using eShopStudying.DataAccess.Repository.IRepository;
 
 namespace eShopStudying.Controllers;
 
@@ -9,15 +10,18 @@ namespace eShopStudying.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
     {
         _logger = logger;
+        _unitOfWork = unitOfWork;
     }
 
     public IActionResult Index()
     {
-        return View();
+        IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
+        return View(productList);
     }
 
     public IActionResult Privacy()
