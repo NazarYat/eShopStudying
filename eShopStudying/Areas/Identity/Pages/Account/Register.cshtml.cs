155c6d832eb704148e5846e3f2506a1a142de7bd
Yatsyuk Nazar <yatsyuk.nazar@gmail.com>
2023-01-09 15:01:13 +0200

    Register form

--- eShopStudying/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ eShopStudying/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -19,6 +19,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using eShopStudying.Utility;
+using eShopStudying.Models;
 
 namespace eShopStudying.Areas.Identity.Pages.Account
 {
@@ -101,6 +102,14 @@ namespace eShopStudying.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            [Required]
+            public string Name { get; set; }
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? State { get; set; }
+            public string? PostalCode { get; set; }
+            public string? PhoneNumber { get; set; }
         }
 
 
@@ -128,6 +137,11 @@ namespace eShopStudying.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.City = Input.City;
+                user.Name = Input.Name;
+                user.PhoneNumber = Input.PhoneNumber;
+                user.PostalCode = Input.PostalCode;
+                user.State = Input.State;
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
@@ -166,11 +180,11 @@ namespace eShopStudying.Areas.Identity.Pages.Account
             return Page();
         }
 
-        private IdentityUser CreateUser()
+        private ApplicationUser CreateUser()
         {
             try
             {
-                return Activator.CreateInstance<IdentityUser>();
+                return Activator.CreateInstance<ApplicationUser>();
             }
             catch
             {
