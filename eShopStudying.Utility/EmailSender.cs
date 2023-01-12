839a9f15e2b34b2c9d6a38da329b2600055cd3fe
Yatsyuk Nazar <yatsyuk.nazar@gmail.com>
2023-01-12 23:42:07 +0200

    Email sending

--- eShopStudying.Utility/EmailSender.cs
+++ eShopStudying.Utility/EmailSender.cs
@@ -1,4 +1,6 @@
+using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
 
 namespace eShopStudying.Utility
 {
@@ -6,6 +8,20 @@ namespace eShopStudying.Utility
     {
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
+            // var emailToSend = new MimeMessage();
+            // emailToSend.From.Add(MailboxAddress.Parse("eShopStudying"));
+            // emailToSend.To.Add(MailboxAddress.Parse(email));
+            // emailToSend.Subject = subject;
+            // emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage};
+
+            // using (var client = new SmtpClient())
+            // {
+            //     client.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+            //     client.Authenticate("email", "password");
+            //     client.Send(emailToSend);
+            //     client.Disconnect(true);
+            // }
+
             return Task.CompletedTask;
         }
     }
